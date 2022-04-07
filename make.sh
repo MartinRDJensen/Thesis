514de72cf6ae509659c6fa856e3d9b3c54a711b7
Martin Jensen <snegelen@Martins-MacBook-Air.local>
2022-04-07 13:33:27 +0200

    Can make share of pk as well as static keygen for test

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 alias run='cd ../MP-SPDZ/; ./RSIG.x; cd ../Thesis/';
-alias mk='cp RSIG/* ../MP-SPDZ/RSIG/; mv fake-stuff.hpp ../MP-SPDZ/Protocols; mv Makefile ../MP-SPDZ; make rsig -C ../MP-SPDZ/';
+alias mk='cp RSIG/* ../MP-SPDZ/RSIG/; cp fake-stuff.hpp ../MP-SPDZ/Protocols; cp Makefile ../MP-SPDZ; make rsig -C ../MP-SPDZ/';
 
 
