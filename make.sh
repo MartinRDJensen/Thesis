6d6d168401da922a94d0ab10b073f1038b64d6ad
Martin Jensen <snegelen@Martins-MacBook-Air.local>
2022-06-15 11:50:31 +0200

    Updates make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-alias build='cd ../MP-SPDZ/; ./Fake-RSIG.x; cd ../Thesis/'
 alias clean='cd ../MP-SPDZ/; make clean; cd ../Thesis/'
-alias run='./run_fake_parties.sh';
 alias mk='cp RSIG/* ../MP-SPDZ/RSIG/; cp fake-stuff.hpp ../MP-SPDZ/Protocols; cp Data_Files.h ../MP-SPDZ/Processor ;cp MascotPrep.* ../MP-SPDZ/Protocols; cp Makefile ../MP-SPDZ; make -j8 rsig -C ../MP-SPDZ/';
 
